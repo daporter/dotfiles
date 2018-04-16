2be077012cf0c42ae61c04ced54cef4aab12653b
David Porter <david.a.porter@gmail.com>
2018-04-16 22:17:23 +1000

    Improve Bash prompt

--- .bashrc.d/prompt.bash
+++ .bashrc.d/prompt.bash
@@ -1,11 +1,37 @@
-# From Homebrew bash-git-prompt:
-if [ -f "/usr/local/opt/bash-git-prompt/share/gitprompt.sh" ]; then
-    __GIT_PROMPT_DIR="/usr/local/opt/bash-git-prompt/share"
-    source "/usr/local/opt/bash-git-prompt/share/gitprompt.sh"
-fi
+#!/bin/bash
 
-function prompt_callback {
-    if [ $(jobs | wc -l) -ne 0 ]; then
-        echo -n " ${Green}jobs:\j"
+RESET="$(tput sgr0)"
+
+BLUE="$(tput setaf 4)"
+GREEN="$(tput setaf 2)"
+GREY="$(tput setaf 244)"
+RED="$(tput setaf 1)"
+YELLOW="$(tput setaf 3)"
+
+git_prompt() {
+    BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/*\(.*\)/\1/')
+
+    if [ ! -z "$BRANCH" ]; then
+        echo -n "$GREEN$BRANCH"
+
+    if [ ! -z "$(git status --short)" ]; then
+        echo " ${RED}✗"
+    fi
+        fi
+}
+
+vim_prompt() {
+    if [ ! -z "$VIMRUNTIME" ]; then
+        echo ":${GREEN}sh ";
     fi
 }
+
+jobs_prompt() {
+    if [ $(jobs -p | wc -l) -ne 0 ]; then
+        echo "${GREY}\j "
+    fi
+}
+
+PS1="
+\[${GREY}\t ${YELLOW}\j${RESET} ${BLUE}\w$(git_prompt)\]
+\[${RESET}\]\$ "
