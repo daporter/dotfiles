90acffbdd3e0d50350dbe906bc9cb4d5b1e243ab
David Porter <david.a.porter@gmail.com>
2018-11-10 09:33:35 +1100

    Add iTerm shell integration functions

---
+++ .profile.d/iterm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Installed by iTerm's "Install Shell Integration" function.
+test -e "${HOME}/.iterm2_shell_integration.bash" && \
+    source "${HOME}/.iterm2_shell_integration.bash"
