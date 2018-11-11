1b3b33f81a4f9f3afbc27224826ba61660ae4d39
David Porter <david.a.porter@gmail.com>
2018-11-11 14:51:17 +1100

    Revert "Add iTerm shell integration functions"
    This reverts commit 90acffbdd3e0d50350dbe906bc9cb4d5b1e243ab.
    
    I'm removing the iTerm2 shell integration because it wasn't working
    properly with Liquid Prompt.

--- .profile.d/iterm.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Installed by iTerm's "Install Shell Integration" function.
-test -e "${HOME}/.iterm2_shell_integration.bash" && \
-    source "${HOME}/.iterm2_shell_integration.bash"
