234db1c8fdec1d116112e6efc1284d636b36f3f2
David Porter <david.a.porter@gmail.com>
2019-07-06 18:02:49 +1000

    Improve the customisation of `ls'
    Also add GNU programs to PATH.

---
+++ .profile.d/gnu.sh
@@ -0,0 +1,6 @@
+# From the coreutils Homebrew package:
+#
+# Commands also provided by MacOS have been installed with the prefix "g".
+# If you need to use these commands with their normal names, you can add a
+# "gnubin" directory to your PATH from your bashrc like:
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
