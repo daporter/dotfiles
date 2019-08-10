a0e6fb31149da621a96fe1227ae57f019e8abb22
David Porter <david@zwolle.home>
2019-08-10 11:43:47 +1000

    Remove MacOS-specific files

--- .profile.d/fasd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-eval "$(fasd --init auto)"
-
-# Use "c" instead of "z" to do directory jumping.
-alias c='fasd_cd -d'
-alias ci='fasd_cd -d -i'
--- .profile.d/mactex.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# MacTEX, installed by Homebrew
-PATH="$PATH:/usr/local/texlive/2018/bin/x86_64-darwin/"
-export PATH
