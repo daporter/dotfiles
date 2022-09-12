d85871de7aa85006df48ccda28047df481f4cf4f
David Porter <david@daporter.net>
2022-09-12 16:27:10 +0200

    emacs: tweak early-init.el

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -4,14 +4,18 @@
 
 ;;; Code:
 
-;; Initialise installed packages
-(setq package-enable-at-startup t)
-
-;; Allow loading from the package cache
-(setq package-quickstart t)
+(setq frame-resize-pixelwise t)
 
 ;; Do not resize the frame at this early stage.
 (setq frame-inhibit-implied-resize t)
+
+;; Initialise installed packages
+(setq package-enable-at-startup t)
+
+(defvar package-quickstart)
+
+;; Allow loading from the package cache
+(setq package-quickstart t)
 
 ;; Disable GUI elements
 (menu-bar-mode -1)
