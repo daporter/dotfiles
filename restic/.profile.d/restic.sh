512860c8c17c96bf19cf1bc0880fca59b39939bd
David Porter <david@daporter.net>
2023-03-22 13:13:08 +0100

    restic: update for switch to Arch

--- restic/.profile.d/restic.sh
+++ restic/.profile.d/restic.sh
@@ -3,5 +3,5 @@
 RESTIC_REPOSITORY=rest:http://beilen:8000/
 export RESTIC_REPOSITORY
 
-RESTIC_PASSWORD_COMMAND="pass restic"
+RESTIC_PASSWORD_COMMAND="op read op://Personal/restic/password"
 export RESTIC_PASSWORD_COMMAND
