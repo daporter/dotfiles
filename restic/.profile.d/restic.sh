dfb1bc53010afad6843f5f9da9997429cba4fa4b
David Porter <david@daporter.net>
2023-08-22 09:14:15 +0200

    restic: move repository to local disk

--- restic/.profile.d/restic.sh
+++ restic/.profile.d/restic.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RESTIC_REPOSITORY=rest:http://beilen:8000/
+RESTIC_REPOSITORY=/mnt/backup
 export RESTIC_REPOSITORY
 
 RESTIC_PASSWORD_COMMAND="gpg -q --for-your-eyes-only --no-tty -d $HOME/.config/restic/restic.gpg"
