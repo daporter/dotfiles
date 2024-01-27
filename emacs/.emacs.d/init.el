067e2c71a1ce44d2d305c19824662a014959f0f5
David Porter <david@daporter.net>
2024-01-27 15:58:29 +0100

    emacs: Tweak hippie-expand-try-functions-list

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -522,15 +522,17 @@ When called interactively without a prefix numeric argument, N is
   :bind
   ("M-/" . hippie-expand)
   :custom
-  (hippie-expand-try-functions-list '(try-expand-dabbrev
-                                      try-expand-line
-                                      try-expand-list
-                                      try-complete-lisp-symbol-partially
+  (hippie-expand-try-functions-list '(try-expand-dabbrev-visible
+                                      try-expand-dabbrev
+                                      try-expand-dabbrev-all-buffers
                                       try-complete-file-name-partially
                                       try-complete-file-name
                                       try-expand-all-abbrevs
+                                      try-expand-list
+                                      try-expand-line
                                       try-expand-dabbrev-from-kill
-                                      try-expand-dabbrev-all-buffers)))
+                                      try-complete-lisp-symbol-partially
+                                      try-complete-lisp-symbol))))
 
 (use-package bookmark
   :defer t
