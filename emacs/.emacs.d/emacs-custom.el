495ef68428d68d9ca42fa719d600284335dd6dcc
David Porter <david@daporter.net>
2024-09-20 12:31:35 +0200

    Merge from master

--- emacs/.emacs.d/emacs-custom.el
+++ emacs/.emacs.d/emacs-custom.el
@@ -3,12 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes t nil nil "Customized with use-package custom")
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
    '("~/Dropbox/63 Programming/63.02 LeetCode C Practice/63.02 LeetCode C Practice Journal.org"))
  '(package-selected-packages
-   '(edit-indirect meow embrace apheleia gptel auth-source-1password org-anki keyfreq expand-region ligature org-modern bind-key cl-generic cl-lib eglot eldoc erc external-completion faceup flymake jsonrpc let-alist map nadvice ntlm org project python so-long soap-client svg tramp use-package verilog-mode xref page-break-lines pcmpl-args embark-consult embark avy nerd-icons-corfu nerd-icons-completion nerd-icons-ibuffer nerd-icons-dired pulsar which-key magit-todos transpose-frame rainbow-mode denote visual-fill-column nov sxhkdrc-mode editorconfig titlecase unfill org-noter pdf-tools olivetti lorem-ipsum flymake-markdownlint cape corfu yaml-mode marginalia orderless vertico adaptive-wrap magit python-mode smart-tabs-mode vterm markdown-mode)))
+   '(embrace apheleia meow gptel auth-source-1password org-anki keyfreq expand-region ligature org-modern bind-key cl-generic cl-lib eglot eldoc external-completion faceup flymake jsonrpc let-alist map nadvice ntlm org project python so-long soap-client svg tramp use-package verilog-mode xref page-break-lines pcmpl-args embark-consult embark avy nerd-icons-corfu nerd-icons-completion nerd-icons-ibuffer nerd-icons-dired pulsar which-key magit-todos transpose-frame rainbow-mode denote visual-fill-column nov sxhkdrc-mode editorconfig titlecase unfill org-noter pdf-tools olivetti lorem-ipsum flymake-markdownlint cape corfu yaml-mode marginalia orderless vertico adaptive-wrap magit python-mode smart-tabs-mode vterm markdown-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
