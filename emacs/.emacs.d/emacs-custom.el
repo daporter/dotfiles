753396694d49d9d5e9144848067bdedfb4217aec
David Porter <david@daporter.net>
2026-08-28 11:34:58 +1000

    emacs: Remove package unfill
    It’s now included is 31.1.

--- emacs/.emacs.d/emacs-custom.el
+++ emacs/.emacs.d/emacs-custom.el
@@ -5,24 +5,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(agent-shell apheleia async auto-compile beframe cape captain
-		 casual-suite consult consult-notmuch corfu
-		 corfu-prescient csv-mode dape doric-themes eglot
-		 embark embark-consult flymake flymake-hledger
-		 flymake-lua flymake-markdownlint flymake-vale
-		 flymake-yamllint fontaine ghostel hl-todo
-		 hyprlang-ts-mode indent-bars ledger-mode ligature
-		 lorem-ipsum lua-mode magit marginalia markdown-mode
-		 nerd-icons nerd-icons-completion nerd-icons-corfu
-		 nerd-icons-dired nerd-icons-grep nerd-icons-ibuffer
-		 nerd-icons-xref olivetti orderless org-anki
-		 org-modern page-break-lines pcmpl-args popper
-		 prescient project python-mode reader shannon-max
-		 string-inflection titlecase tramp unfill vertico
-		 vertico-prescient visual-fill-column vterm))
+   '(agent-shell apheleia async auto-compile beframe cape captain casual-suite
+                 consult consult-notmuch corfu corfu-prescient csv-mode dape
+                 doric-themes eglot embark embark-consult flymake
+                 flymake-hledger flymake-lua flymake-markdownlint flymake-vale
+                 flymake-yamllint fontaine ghostel hl-todo hyprlang-ts-mode
+                 indent-bars ledger-mode ligature lorem-ipsum lua-mode magit
+                 marginalia markdown-mode nerd-icons nerd-icons-completion
+                 nerd-icons-corfu nerd-icons-dired nerd-icons-grep
+                 nerd-icons-ibuffer nerd-icons-xref olivetti orderless org-anki
+                 org-modern page-break-lines pcmpl-args popper prescient project
+                 python-mode reader shannon-max string-inflection titlecase
+                 tramp vertico vertico-prescient visual-fill-column vterm))
  '(package-vc-selected-packages
    '((reader :url "https://codeberg.org/MonadicSheep/emacs-reader" :make
-	     "clean all")
+             "clean all")
      (flymake-vale :url "https://github.com/tpeacock19/flymake-vale")
      (shannon-max :url "https://github.com/sstraust/shannonmax"))))
 (custom-set-faces
